a62635a14bcd6a188ce5674dcbecb40ae0df4d5d
SunXiaobei <1371815174@qq.com>
2022-08-06 14:39:11 +0000

    create function units

---
+++ workspace/hand_painting/etc/flowunit/yolox_post/yolox_post.py
@@ -0,0 +1,49 @@
+# Copyright (c) Huawei Technologies Co., Ltd. 2022. All rights reserved.
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import _flowunit as modelbox
+
+class yolox_postFlowUnit(modelbox.FlowUnit):
+    # Derived from modelbox.FlowUnit
+    def __init__(self):
+        super().__init__()
+
+    def open(self, config):
+        # Open the flowunit to obtain configuration information
+        return modelbox.Status.StatusCode.STATUS_SUCCESS
+
+    def process(self, data_context):
+        # Process the data
+        in_data = data_context.input("in_1")
+        out_data = data_context.output("out_1")
+
+        # yolox_post process code.
+        # Remove the following code and add your own code here.
+        for buffer in in_data:
+            response = "Hello World " + buffer.as_object()
+            result = response.encode('utf-8').strip()
+            add_buffer = modelbox.Buffer(self.get_bind_device(), result)
+            out_data.push_back(add_buffer)
+
+        return modelbox.Status.StatusCode.STATUS_SUCCESS
+
+    def close(self):
+        # Close the flowunit
+        return modelbox.Status()
+
+    def data_pre(self, data_context):
+        # Before streaming data starts
+        return modelbox.Status()
+
+    def data_post(self, data_context):
+        # After streaming data ends
+        return modelbox.Status()
+
+    def data_group_pre(self, data_context):
+        # Before all streaming data starts
+        return modelbox.Status()
+
+    def data_group_post(self, data_context):
+        # After all streaming data ends
+        return modelbox.Status()
