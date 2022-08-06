cff9a428d55d37caeb54e7c3c3e4a2b275c49d8e
SunXiaobei <1371815174@qq.com>
2022-08-06 04:38:00 +0000

    modify draw_text

--- workspace/hello_world/etc/flowunit/draw_text/draw_text.py
+++ workspace/hello_world/etc/flowunit/draw_text/draw_text.py
@@ -4,6 +4,9 @@
 # -*- coding: utf-8 -*-
 import _flowunit as modelbox
 
+import cv2
+import numpy as np
+
 class draw_textFlowUnit(modelbox.FlowUnit):
     # Derived from modelbox.FlowUnit
     def __init__(self):
@@ -21,10 +24,20 @@ class draw_textFlowUnit(modelbox.FlowUnit):
         # draw_text process code.
         # Remove the following code and add your own code here.
         for buffer in in_data:
-            response = "Hello World " + buffer.as_object()
-            result = response.encode('utf-8').strip()
-            add_buffer = modelbox.Buffer(self.get_bind_device(), result)
-            out_data.push_back(add_buffer)
+            # 获取视频信息
+            width = buffer.get('width')
+            height = buffer.get('height')
+            channel = buffer.get('channel')
+            img_data = np.array(buffer.as_object(), copy=False)
+            img_data = img_data.reshape((height, width, channel))
+            cv2.putText(img_data, 'Hello World', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            out_buffer = self.create_buffer(img_data)
+            out_buffer.copy_meta(buffer)
+            out_data.push_back(out_buffer)
+            # response = "Hello World " + buffer.as_object()
+            # result = response.encode('utf-8').strip()
+            # add_buffer = modelbox.Buffer(self.get_bind_device(), result)
+            # out_data.push_back(add_buffer)
 
         return modelbox.Status.StatusCode.STATUS_SUCCESS
 
